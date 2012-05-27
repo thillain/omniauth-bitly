15e91494a0a0c3e813ede3604a0ad3398c02d460
JudeArasu <judearasu@gmail.com>
2012-05-27 13:14:17 +0530

    bitly structure

---
+++ lib/omniauth-bitly.rb
@@ -0,0 +1,2 @@
+require "omniauth-bitly/version"
+require 'omniauth/strategies/bitly'
---
+++ lib/omniauth/strategies/bitly.rb
@@ -0,0 +1,10 @@
+require 'omniauth-oauth2'
+require 'multi_json'
+
+module OmniAuth
+  module Strategies
+    class Bitly < OmniAuth::Strategies::OAuth2
+      
+    end
+  end
+end
