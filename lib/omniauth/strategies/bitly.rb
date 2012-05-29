0cb90e34a467a2f39a718483e551b34fb97f3a12
JudeArasu <judearasu@gmail.com>
2012-05-29 06:06:43 +0530

    raw info added

--- lib/omniauth/strategies/bitly.rb
+++ lib/omniauth/strategies/bitly.rb
@@ -6,7 +6,30 @@ module OmniAuth
     class Bitly < OmniAuth::Strategies::OAuth2
       option :name, "bitly"
       option :client_options,{:site=>'https://bitly.com',
-                              :authorize_url => '/oauth/authorize',}
+                              :authorize_url => '/oauth/authorize',
+                              :token_url => 'https://api-ssl.bit.ly/oauth/access_token'}
+      uid{ raw_info['id'] }
+
+      info do
+        prune!({
+          'login' => raw_info['login'],
+          'display_name' => raw_info['display_name'],
+          'full_name' => raw_info['full_name'],
+          'profile_image' => raw_info['profile_image'],
+          'profile_ur'=>raw_info['profile_url'],
+          'urls' => {
+            'Bitly' => raw_info['link'],
+            'Website' => raw_info['website']
+          }
+           })
+      end
+      extra do
+        {:raw_info => raw_info}
+      end
+      def raw_info
+          @raw_info ||= access_token.get("https://api-ssl.bitly.com/v3/user/").parsed['response']
+      end
     end
   end
 end
+OmniAuth.config.add_camelization 'bitly', 'Bitly'
